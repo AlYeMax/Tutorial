201ec7fa60482d09456ca75bf6b3a813ca53e04c
AlYeMax <49675625+AlYeMax@users.noreply.github.com>
2023-08-03 11:28:26 +0300

    2

--- index.js
+++ index.js
@@ -1,7 +1,35 @@
-a
-a[title="link"]
-.submenu
-#title
-.social-links a
-<h1 class="title accent"></h1>
-<h2 class="title"></h2>
+// a
+// a[title="link"]
+// .submenu
+// #title
+// .social-links a
+// <h1 class="title accent"></h1>
+// <h2 class="title"></h2>
+// console.log("class:", class)
+// const apples = 47;
+// const grapes = 135;
+// const total = apples + grapes;
+// console.log("total:", total)
+// console.log("total:", total);
+// const Name = 'Filya';
+// console.log("Name:", Name);
+
+const companyName = 'Cyber Systems';
+const repairBots = 150;
+const defenceBots = 50;
+const massage = `${companyName} has ${repairBots + defenceBots} bots in stock`
+console.log(massage); // "Cyber Systems has 200 bots in stock"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
